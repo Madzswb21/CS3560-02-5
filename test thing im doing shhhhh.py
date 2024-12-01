6f244706f8ae3dab2b9e42ac1849d9a6710c77d8
Madison Smith <jeffswb21@gmail.com>
2024-11-30 22:59:36 -0800

    Update test thing im doing shhhhh.py

--- test thing im doing shhhhh.py
+++ test thing im doing shhhhh.py
@@ -11,44 +11,31 @@ class cafeGUI:
         
 
         # labels for navigation bar
-        self.searchLabel = tk.Label(self.root, text="search")
+        self.searchLabel = tk.Label(self.root, text="Username:")
         self.searchBox = tk.Entry(self.root)
         self.naviBar()
 
-        self.buttonDrink = tk.Button(text="Drink", width=5,font=("Times New Roman",14))
-        self.buttonFood = tk.Button(text="Food", width=5,font=("Times New Roman",14))
-        self.buttonOther = tk.Button(text="Other", width=5,font=("Times New Roman",14))
-
-
-        self.buttonItem = tk.Button(text="Item", width=10,font=("Times New Roman",14))
-        self.buttonItem2 = tk.Button(text="Item", width=10,font=("Times New Roman",14))
-        self.buttonItem3 = tk.Button(text="Item", width=10,font=("Times New Roman",14))
-
-        self.buttonaddto1 = tk.Button(text="add", width=10,font=("Times New Roman",10))
+     #   self.buttonDrink = tk.Button(text="Drink", width=5,font=("Times New Roman",14))
+      #  self.buttonFood = tk.Button(text="Food", width=5,font=("Times New Roman",14))
 
 
 
 
-        self.button()
+
+       # self.button()
 
 
 
         self.root.mainloop()
     
     def naviBar(self):
-        self.searchLabel.grid(row = 0, column = 0, rowspan=5, pady = 2)
-        self.searchBox.grid(row = 0, column = 1, pady = 2)
+        self.searchLabel.grid(row = 8, column = 9)
+       # self.searchBox.grid(row = 9, column = 9, rowspan=5, pady = 2)
     
-    def button(self):
-        self.buttonDrink.grid(row = 5, column = 0, pady = 6)
-        self.buttonFood.grid(row = 5, column = 1, pady = 6)
-        self.buttonOther.grid(row = 5, column = 2, pady = 6)
-        self.buttonItem.grid(row = 7, column = 0, pady = 50)
-        self.buttonItem2.grid(row = 7, column = 1, pady = 50)
-        self.buttonItem3.grid(row = 7, column = 2, pady = 50)
-
-        self.buttonaddto1.grid(row = 8, column = 0, pady = 20)
-
+    #def button(self):
+       # self.buttonDrink.grid(row = 10, column = 10, pady = 6)
+        #self.buttonFood.grid(row = 10, column = 10, pady = 6)
+        
 
 
 
