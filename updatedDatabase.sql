90fa4bf5d3fae9de94faa589855de5a5b698b2ad
Quynh Lam <lamq4473@gmail.com>
2024-12-01 02:12:22 -0800

    update database

--- updatedDatabase.sql
+++ updatedDatabase.sql
@@ -1,11 +1,14 @@
--- DROP DATABASE IF EXISTS cafeRestaurant; -- use this query if you want to delete old database and make new one with changes applied
+DROP DATABASE IF EXISTS cafeRestaurant; -- use this query if you want to delete old database 
+										   -- and make new one with changes applied
 CREATE DATABASE cafeRestaurant;
 -- make sure to uncomment before pushing :)
 
--- USE cafeRestaurant; -- use this query if you encounter error where db is created with no tables
+USE cafeRestaurant; -- use this query if you encounter error where db is created with no tables
 -- creates a table to hold information on staff members
 CREATE TABLE IF NOT EXISTS Staff (
 	staffID int PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(20),
+    pass VARCHAR(255),   -- space to hash the password
     f_name VARCHAR(45),
     l_name VARCHAR(45),
     emailAddress VARCHAR(45),
@@ -17,6 +20,8 @@ ALTER TABLE Staff AUTO_INCREMENT = 1000; -- sets the auto-generated IDs to start
 -- creates a table to hold information on customers
 CREATE TABLE IF NOT EXISTS Customer (
 	custID int PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(20),
+    pass VARCHAR(255),   -- space to hash the password
     f_name VARCHAR(45),
     l_name VARCHAR(45),
     emailAddress VARCHAR(45),
@@ -75,6 +80,7 @@ CREATE TABLE IF NOT EXISTS MenuItem (
 -- new changes, run this 1st
 ALTER TABLE MenuItem CHANGE isInStock stock int; -- change name to stock and use quantity to show whether an item is available or not
 ALTER TABLE MenuItem ADD category ENUM ("food", "drink", "other"); -- category column with 3 choices
+ALTER TABLE MenuItem ADD image VARCHAR(45);
 
 -- creates a table that holds items in a specifc order
 -- takes in a menu item ID and an existing order ID to create a compound key
@@ -83,6 +89,7 @@ CREATE TABLE IF NOT EXISTS ItemsInOrder (
 	menuItemID int,
     orderID int,
     quantity int,
+    customization VARCHAR(45), 
     FOREIGN KEY(menuItemID) REFERENCES MenuItem(menuItemID),
     FOREIGN KEY(orderID) REFERENCES Orders(orderID),
     PRIMARY KEY(menuItemID, orderID)
@@ -91,55 +98,63 @@ CREATE TABLE IF NOT EXISTS ItemsInOrder (
 -- dummy variables for testing purposes; for "customer," "staff" and "menu item"
 -- make sure to comment out after running once if needed, don't want duplicates
 -- staff members
-INSERT INTO Staff (f_name, l_name, emailAddress, phoneNumber, staffRole) VALUES 
-	("Jane", "Doe", "janedoe@email.com", "123-456-7890", "Server");
-INSERT INTO Staff (f_name, l_name, emailAddress, phoneNumber, staffRole) VALUES 
-	("John", "Doe", "johndoe@email.com", "111-222-3434", "Cashier");
+INSERT INTO Staff (username, pass, f_name, l_name, emailAddress, phoneNumber, staffRole) VALUES 
+	("staff", "1234", "Jane", "Doe", "janedoe@email.com", "123-456-7890", "Server");
+INSERT INTO Staff (username, pass, f_name, l_name, emailAddress, phoneNumber, staffRole) VALUES 
+	("employee", "0000", "John", "Doe", "johndoe@email.com", "111-222-3434", "Cashier");
 
 -- customers
-INSERT INTO Customer(f_name, l_name, emailAddress, phoneNumber, physAddress) VALUES
-	("Caoilainn", "Johnsson", "cj@email.com", "222-222-2222", "222 Some St.");
-INSERT INTO Customer(f_name, l_name, emailAddress, phoneNumber, physAddress) VALUES 
-	("Joe", "Mama", "jm@email.com", "555-555-5555", "555 Some St.");
+INSERT INTO Customer(username, pass, f_name, l_name, emailAddress, phoneNumber, physAddress) VALUES
+	("cjohnsson", "55555", "Caoilainn", "Johnsson", "cj@email.com", "222-222-2222", "222 Some St.");
+INSERT INTO Customer(username, pass, f_name, l_name, emailAddress, phoneNumber, physAddress) VALUES 
+	("jmama", "22222", "Joe", "Mama", "jm@email.com", "555-555-5555", "555 Some St.");
 
 -- menu items
-INSERT INTO MenuItem (itemName, itemDesc, itemPrice, isInStock, calories) VALUES 
-	("Turkey BLT", "Turkey sandwich on sourdough with bacon, lettuce and tomato with garlic aoili", 12.99, True, 447);
-INSERT INTO MenuItem (itemName, itemDesc, itemPrice, isInStock, calories) VALUES 
-	("Egg Salad Sandwich", "Egg salad on rye bread", 11.99, True, 350);
-INSERT INTO MenuItem (itemName, itemDesc, itemPrice, isInStock, calories) VALUES 
-	("Caeser Salad", "Romaine lettuce with house-made Caeser dressing, topped with grilled chicken", 14.00, True, 550);
-INSERT INTO MenuItem (itemName, itemDesc, itemPrice, isInStock, calories) VALUES 
-	("French Onion Soup", "House-made french onion soup, served in a bread bowl", 14.50, True, 600);
-INSERT INTO MenuItem (itemName, itemDesc, itemPrice, isInStock, calories) VALUES 
-	("Chicken Noodle Soup", "House-made chicken noodle soup with carrots and celery", 13.50, True, 500);
+INSERT INTO MenuItem (itemName, itemDesc, itemPrice, stock, calories, category) VALUES 
+	("Turkey BLT", "Turkey sandwich on sourdough with bacon, lettuce and tomato with garlic aoili", 12.99, 10, 447, "food");
+INSERT INTO MenuItem (itemName, itemDesc, itemPrice, stock, calories, category) VALUES 
+	("Egg Salad Sandwich", "Egg salad on rye bread", 11.99, 10, 350, "food");
+INSERT INTO MenuItem (itemName, itemDesc, itemPrice, stock, calories, category) VALUES 
+	("Caeser Salad", "Romaine lettuce with house-made Caeser dressing, topped with grilled chicken", 14.00, 10, 550, "food");
+INSERT INTO MenuItem (itemName, itemDesc, itemPrice, stock, calories, category) VALUES 
+	("French Onion Soup", "House-made french onion soup, served in a bread bowl", 14.50, 10, 600, "food");
+INSERT INTO MenuItem (itemName, itemDesc, itemPrice, stock, calories, category) VALUES 
+	("Chicken Noodle Soup", "House-made chicken noodle soup with carrots and celery", 13.50, 10, 500, "food");
 
-INSERT INTO MenuItem (itemName, itemDesc, itemPrice, isInStock, calories) VALUES 
-	("Vanilla Latte", "Vanilla latte with fresh ground espresso", 5.50, True, 250);
-INSERT INTO MenuItem (itemName, itemDesc, itemPrice, isInStock, calories) VALUES 
-	("Matcha Latte", "Ceremonial-grade matcha with milk", 6.50, True, 225);
-INSERT INTO MenuItem (itemName, itemDesc, itemPrice, isInStock, calories) VALUES 
-	("Coffee", "Freshly brewed coffee with 100% Arabica beans", 2.00, True, 10);
-INSERT INTO MenuItem (itemName, itemDesc, itemPrice, isInStock, calories) VALUES 
-	("Green tea", "Steeped green tea leaves", 2.00, True, 10);
-INSERT INTO MenuItem (itemName, itemDesc, itemPrice, isInStock, calories) VALUES 
-	("Mocha Latte", "Chcolate and espresso combined with milk", 5.50, True, 280);
+INSERT INTO MenuItem (itemName, itemDesc, itemPrice, stock, calories, category) VALUES 
+	("Vanilla Latte", "Vanilla latte with fresh ground espresso", 5.50, 10, 250, "drink");
+INSERT INTO MenuItem (itemName, itemDesc, itemPrice, stock, calories, category) VALUES 
+	("Matcha Latte", "Ceremonial-grade matcha with milk", 6.50, 10, 225, "drink");
+INSERT INTO MenuItem (itemName, itemDesc, itemPrice, stock, calories, category) VALUES 
+	("Coffee", "Freshly brewed coffee with 100% Arabica beans", 2.00, 10, 10, "drink");
+INSERT INTO MenuItem (itemName, itemDesc, itemPrice, stock, calories, category) VALUES 
+	("Green tea", "Steeped green tea leaves", 2.00, 10, 10, "drink");
+INSERT INTO MenuItem (itemName, itemDesc, itemPrice, stock, calories, category) VALUES 
+	("Mocha Latte", "Chcolate and espresso combined with milk", 5.50, 10, 280, "drink");
 
-INSERT INTO MenuItem (itemName, itemDesc, itemPrice, isInStock, calories) VALUES 
-	("Croissant", "Flaky, buttery pastery", 2.00, True, 150);
-INSERT INTO MenuItem (itemName, itemDesc, itemPrice, isInStock, calories) VALUES 
-	("Side of Fries", "Crinkle-cut fries made fresh-to-order", 3.50, True, 250);
-INSERT INTO MenuItem (itemName, itemDesc, itemPrice, isInStock, calories) VALUES 
-	("Side Salad", "House Salad with balsamic dressing", 4.00, True, 150);
-INSERT INTO MenuItem (itemName, itemDesc, itemPrice, isInStock, calories) VALUES 
-	("Bleuberry Muffin", "Freshly baked blueberry muffin", 2.50, True, 300);
-INSERT INTO MenuItem (itemName, itemDesc, itemPrice, isInStock, calories) VALUES 
-	("Brownie", "Freshly baked fudge brownie dusted with powdered sugar", 2.99, True, 300);
+INSERT INTO MenuItem (itemName, itemDesc, itemPrice, stock, calories, category) VALUES 
+	("Croissant", "Flaky, buttery pastery", 2.00, 10, 150, "other");
+INSERT INTO MenuItem (itemName, itemDesc, itemPrice, stock, calories, category) VALUES 
+	("Side of Fries", "Crinkle-cut fries made fresh-to-order", 3.50, 10, 250, "other");
+INSERT INTO MenuItem (itemName, itemDesc, itemPrice, stock, calories, category) VALUES 
+	("Side Salad", "House Salad with balsamic dressing", 4.00, 10, 150, "other");
+INSERT INTO MenuItem (itemName, itemDesc, itemPrice, stock, calories, category) VALUES 
+	("Bleuberry Muffin", "Freshly baked blueberry muffin", 2.50, 10, 300, "other");
+INSERT INTO MenuItem (itemName, itemDesc, itemPrice, stock, calories, category) VALUES 
+	("Brownie", "Freshly baked fudge brownie dusted with powdered sugar", 2.99, 10, 300, "other");
 
--- populate stock and category column, run this 2nd
-UPDATE MenuItem SET stock = 5 WHERE menuItemID > 0;
-UPDATE MenuItem SET category = "food" WHERE menuItemID < 6;
-UPDATE MenuItem SET category = "drink" WHERE menuItemID BETWEEN 6 AND 10;
-UPDATE MenuItem SET category = "other" WHERE menuItemID BETWEEN 11 AND 15;
+
+-- test img 
+UPDATE MenuItem SET image = "img/turkey.png" WHERE menuItemID = 1;
+UPDATE MenuItem SET image = "img/eggsandwich.png" WHERE menuItemID = 2;
+UPDATE MenuItem SET image = "img/vanillalatte.png" WHERE menuItemID = 6;
+UPDATE MenuItem SET image = "img/matchalatte.png" WHERE menuItemID = 7;
+UPDATE MenuItem SET image = "img/croissant.png" WHERE menuItemID = 11;
+UPDATE MenuItem SET image = "img/fries.png" WHERE menuItemID = 12;
+
+
+
+
+
 
 
